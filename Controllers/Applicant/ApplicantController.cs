8cf633b4e9db6b7977e4a9651e8756c8b9fedc0c
isooterkin <isooterkin@outlook.com>
2022-04-13 20:12:21 +0300

    Ахуеенноно поучилось

--- Controllers/Applicant/ApplicantController.cs
+++ Controllers/Applicant/ApplicantController.cs
@@ -20,14 +20,20 @@ namespace EasyToEnter.ASP.Controllers.Applicant
         // Открытие страницы
         public IActionResult Index(int? level = null, int? science = null, int? group = null, int? direction = null)
         {
-            IEnumerable<LevelFocusModel> focuss = _context.LevelGroup
+            List<LevelFocusModel> focuss = _context.LevelGroup
                 .Include(l => l.LevelModel)
                 .Include(lf => lf.FocusModel)
                 .ThenInclude(f => f.DirectionModel)
                 .ThenInclude(d => d.GroupModel)
-                .ThenInclude(s => s.ScienceModel);
+                .ThenInclude(s => s.ScienceModel)
+                .ToList();
 
-            List<SelectListItem> levels = _context.Level.Select(l => new SelectListItem
+            if (level != null) focuss = focuss.Where(l => l.LevelModel.Id == level).ToList();
+            if (science != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.GroupModel.ScienceModel.Id == science).ToList();
+            if (group != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.GroupModel.Id == group).ToList();
+            if (direction != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.Id == direction).ToList();
+
+            List<SelectListItem> levels = focuss.Select(g => g.LevelModel).Distinct().Select(l => new SelectListItem
             {
                 Value = l.Id.ToString(),
                 Text = l.Name
@@ -35,7 +41,7 @@ namespace EasyToEnter.ASP.Controllers.Applicant
             if (levels.Any(l => l.Value == level.ToString())) levels.First(l => l.Value == level.ToString()).Selected = true;
             ViewData["Levels"] = levels;
 
-            List<SelectListItem> sciences = _context.Science.Select(l => new SelectListItem
+            List<SelectListItem> sciences = focuss.Select(g => g.FocusModel.DirectionModel.GroupModel.ScienceModel).Distinct().Select(l => new SelectListItem
             {
                 Value = l.Id.ToString(),
                 Text = l.Name
@@ -43,28 +49,23 @@ namespace EasyToEnter.ASP.Controllers.Applicant
             if (sciences.Any(l => l.Value == science.ToString())) sciences.First(l => l.Value == science.ToString()).Selected = true;
             ViewData["Sciences"] = sciences;
 
-            List<SelectListItem> groups = _context.Group.Select(l => new SelectListItem
+            List<SelectListItem> groups = focuss.Select(g => g.FocusModel.DirectionModel.GroupModel).Distinct().Select(l => new SelectListItem
             {
                 Value = l.Id.ToString(),
-                Text = l.Name,
+                Text = l.Name
             }).ToList();
             if (groups.Any(l => l.Value == group.ToString())) groups.First(l => l.Value == group.ToString()).Selected = true;
             ViewData["Groups"] = groups;
 
-            List<SelectListItem> directions = _context.Direction.Select(l => new SelectListItem
+            List<SelectListItem> directions = focuss.Select(g => g.FocusModel.DirectionModel).Distinct().Select(l => new SelectListItem
             {
                 Value = l.Id.ToString(),
-                Text = l.Name,
+                Text = l.Name
             }).ToList();
             if (directions.Any(l => l.Value == direction.ToString())) directions.First(l => l.Value == direction.ToString()).Selected = true;
             ViewData["Directions"] = directions;
 
-            if (level != null) focuss = focuss.Where(l => l.LevelModel.Id == level);
-            if (science != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.GroupModel.ScienceModel.Id == science);
-            if (group != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.GroupModel.Id == group);
-            if (direction != null) focuss = focuss.Where(l => l.FocusModel.DirectionModel.Id == direction);
-
-            ViewData["Focuss"] = focuss.ToList();
+            ViewData["Focuss"] = focuss;
 
             return View();
         }
