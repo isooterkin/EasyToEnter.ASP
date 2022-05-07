d11c43ee66c5042a96439cea0d8ac96d783a4f2b
isooterkin <isooterkin@outlook.com>
2022-05-07 17:14:20 +0300

    Убрал ненужное.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,46 +8,10 @@ namespace EasyToEnter.ASP.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly EasyToEnterDbContext _context;
-
-        public HomeController(EasyToEnterDbContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult Index()
         {
-            // Вывести группы Бакалавра
-            var allGroup = _context.LevelFocus.Where(l => l.LevelId == 1)
-                .Include(lf => lf.FocusModel)
-                .ThenInclude(f => f.DirectionModel)
-                .ThenInclude(d => d.GroupModel)
-                .Select(g => g.FocusModel.DirectionModel.GroupModel.Name)
-                .Distinct()
-                .ToList();
-
             return View();
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         public IActionResult Privacy()
         {
             return View();
