97db7644427521a3a9ff15fac7540c4a83cf42ef
isooterkin <isooterkin@outlook.com>
2022-05-15 23:02:45 +0300

    Настроил фильтр

--- Controllers/Applicant/FocusSelection.cs
+++ Controllers/Applicant/FocusSelection.cs
@@ -15,54 +15,6 @@ namespace EasyToEnter.ASP.Controllers.Applicant
         {
             if ((level <= 0 || direction <= 0) || (area != null && area <= 0)) return NotFound();
 
-            //List <LevelFocusModel> levelFocusList = await _context.LevelFocus
-            //    .Where(l => l.LevelId == level)
-            //    .Include(l => l.LevelModel)
-            //    .Include(lf => lf.FocusModel)
-            //        .ThenInclude(f => f!.DirectionModel)
-            //    .Where(l => l.FocusModel!.DirectionId == direction)
-            //    .Include(lf => lf.FocusModel)
-            //        .ThenInclude(f => f!.DirectionModel)
-            //            .ThenInclude(f => f!.GroupModel)
-            //    .Include(lf => lf.FocusModel)
-            //        .ThenInclude(lf => lf!.AreaFocuss)
-            //            !.ThenInclude(lf => lf!.AreaModel)
-            //    .ToListAsync();
-
-
-            //IEnumerable<VariabilityModel> variabilityCollection = _context.Variability
-            //    .Include(v => v.FormatModel)
-            //    .Include(v => v.FormModel)
-            //    .Include(v => v.PaymentModel)
-            //    .Include(v => v.FocusUniversityModel)
-            //        .ThenInclude(fu => fu!.LevelFocusModel)
-            //            .ThenInclude(lf => lf!.LevelModel)
-            //    .Where(v => v.FocusUniversityModel!.LevelFocusModel!.LevelId == level)
-            //    .Include(v => v.FocusUniversityModel)
-            //        .ThenInclude(fu => fu!.LevelFocusModel)
-            //            .ThenInclude(lf => lf!.FocusModel)
-            //                .ThenInclude(f => f!.DirectionModel)
-            //                    .ThenInclude(d => d!.GroupModel)
-            //    .Where(v => v.FocusUniversityModel!.LevelFocusModel!.FocusModel!.DirectionId == direction);
-
-            //IEnumerable<FocusUniversityModel> focusUniversityCollection = _context.FocusUniversity
-            //    .Include(f => f.PaymentModel)
-            //    .Include(f => f.FormModel)
-            //    .Include(f => f.FormatModel)
-            //    .Include(f => f.UniversityModel)
-            //    .Include(f => f.LevelFocusModel)
-            //        .ThenInclude(f => f!.LevelModel)
-            //    .Where(f => f.LevelFocusModel!.LevelId == level)
-            //    .Include(f => f.LevelFocusModel)
-            //        .ThenInclude(f => f!.FocusModel)
-            //            .ThenInclude(f => f!.DirectionModel)
-            //                .ThenInclude(f => f!.GroupModel)
-            //    .Where(f => f.LevelFocusModel!.FocusModel!.DirectionId == direction);
-
-            // Все возможные области
-            //IEnumerable<AreaFocusModel> areaFocusList = levelFocusList
-            //    .SelectMany(f => f.FocusModel!.AreaFocuss!);
-
             IEnumerable<FocusUniversityModel> focusUniversityCollection = await _context.FocusUniversity
                 .Include(fu => fu.LevelFocusModel)
                     .ThenInclude(lf => lf!.LevelModel)
@@ -82,28 +34,39 @@ namespace EasyToEnter.ASP.Controllers.Applicant
             IEnumerable<AreaFocusModel> areaFocusCollection = focusUniversityCollection
                 .SelectMany(f => f.LevelFocusModel!.FocusModel!.AreaFocuss!);
 
-            // Конвертация областей в фильтры
-            List<SelectListItem> areaFocusSelectList = areaFocusCollection
-                .Select(af => af.AreaModel!)
-                .Distinct()
-                .Select(a => new SelectListItem(a.Name, a.Id.ToString()))
-                .ToList();
-
+            // Если указан фильтр областей, фильтруем
             if (area != null)
             {
-                var inContains = areaFocusCollection.Where(a => a.AreaModel!.Id == area).First();
-                
-                focusUniversityCollection = focusUniversityCollection
-                    .Where(f => f.LevelFocusModel!.FocusModel!.AreaFocuss!.Contains(inContains));
+                // Что выбрали
+                AreaFocusModel? selectAreaFocus = areaFocusCollection.FirstOrDefault(a => a.AreaModel!.Id == area);
 
-                if (areaFocusSelectList.Any(l => l.Value == area.ToString()))
-                    areaFocusSelectList.First(l => l.Value == area.ToString()).Selected = true;
+                if (selectAreaFocus == null) return NotFound();
+
+                focusUniversityCollection = focusUniversityCollection
+                    .Where(f => f.LevelFocusModel!.FocusModel!.AreaFocuss!.Select(af => af.AreaModel).Contains(selectAreaFocus.AreaModel));
             }
 
+            // Все направленности
             List<LevelFocusModel> levelFocusList = focusUniversityCollection
                 .Select(fu => fu.LevelFocusModel!)
                 .Distinct()
                 .ToList();
+
+            // Конвертация областей в фильтры
+            List<SelectListItem> areaFocusSelectList = areaFocusCollection
+                .Select(af => af.AreaModel!)
+                .Distinct()
+                .Select(a => new SelectListItem($"{a.Name}", $"{a.Id}"))
+                .ToList();
+
+            // .Select(a => new SelectListItem($"{a.Name} ({focusUniversityCollection.Where(lf => lf.LevelFocusModel!.FocusModel!.AreaFocuss!.Contains(areaFocusCollection.Where(r => r.AreaModel!.Id == a.Id).First())).Count()})", a.Id.ToString()))
+
+            // Если выбран какой либо фильтр, устанавливаем его
+            if (area != null)
+            {
+                if (areaFocusSelectList.Any(l => l.Value == area.ToString()))
+                    areaFocusSelectList.First(l => l.Value == area.ToString()).Selected = true;
+            }
 
             return View(new FocusSelectionContainerViewModel(levelFocusList, focusUniversityCollection, areaFocusSelectList, level, direction));
         }
