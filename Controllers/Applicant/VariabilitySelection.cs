e59fe44bc01e2fea6c7cd56b9305718064ad8f7e
isooterkin <isooterkin@outlook.com>
2022-05-16 11:40:00 +0300

    Фиксация

--- Controllers/Applicant/VariabilitySelection.cs
+++ Controllers/Applicant/VariabilitySelection.cs
@@ -1,15 +1,25 @@
 ﻿using EasyToEnter.ASP.Models.Models;
 using EasyToEnter.ASP.ViewsModels.Applicant;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EasyToEnter.ASP.Controllers.Applicant
 {
     public partial class ApplicantController
     {
-        public IActionResult VariabilitySelection([FromQuery(Name = "levelFocus")] int levelFocus)
+        public IActionResult VariabilitySelection(
+            [FromQuery(Name = "levelFocus")] int levelFocus,
+            [FromQuery(Name = "form")] int? form,
+            [FromQuery(Name = "format")] int? format,
+            [FromQuery(Name = "payment")] int? payment
+            )
         {
-            if (levelFocus <= 0) return NotFound();
+            if ((levelFocus <= 0) || 
+                (form != null && form <= 0) || 
+                (format != null && format <= 0) || 
+                (payment != null && payment <= 0)) 
+                return NotFound();
 
             List<VariabilityModel> variabilityList = _context.Variability
                 .Include(v => v.FormModel)
@@ -39,7 +49,51 @@ namespace EasyToEnter.ASP.Controllers.Applicant
                 .Where(v => v.FocusUniversityModel!.LevelFocusId == levelFocus)
                 .ToList();
 
-            return View(new VariabilitySelectionContainerViewModel(variabilityList, levelFocus));
+            // Список форм
+            List<FormModel> formList = variabilityList
+                .Select(v => v.FormModel!)
+                .Distinct()
+                .ToList();
+
+            // Список форматов
+            List<FormatModel> formatList = variabilityList
+                .Select(v => v.FormatModel!)
+                .Distinct()
+                .ToList();
+
+            // Список оплат
+            List<PaymentModel> paymentList = variabilityList
+                .Select(v => v.PaymentModel!)
+                .Distinct()
+                .ToList();
+
+            // Список вступительных экзаменов
+            List<string> entranceExamsList = variabilityList
+                .Select(v => v.EntranceExams == true ? "Да" : "Нет")
+                .Distinct()
+                .ToList();
+
+            // Конвертация форм в фильтры
+            List<SelectListItem> formSelectList = formList
+                .Select(f => new SelectListItem($"{f.Name}", $"{f.Id}"))
+                .ToList();
+
+            // Конвертация форматов в фильтры
+            List<SelectListItem> formatSelectList = formatList
+                .Select(f => new SelectListItem($"{f.Name}", $"{f.Id}"))
+                .ToList();
+
+            // Конвертация форматов в фильтры
+            List<SelectListItem> paymentSelectList = paymentList
+                .Select(f => new SelectListItem($"{f.Name}", $"{f.Id}"))
+                .ToList();
+
+            // Конвертация форматов в фильтры
+            List<SelectListItem> entranceExamsSelectList = entranceExamsList
+                .Select(f => new SelectListItem($"{f}", $"{(f == "Да" ? 1 : 0)}"))
+                .ToList();
+
+            return View(new VariabilitySelectionContainerViewModel(variabilityList, formSelectList, formatSelectList, paymentSelectList, entranceExamsSelectList, levelFocus));
         }
     }
 }
--- ViewsModels/Applicant/VariabilitySelectionContainerViewModel.cs
+++ ViewsModels/Applicant/VariabilitySelectionContainerViewModel.cs
@@ -1,15 +1,24 @@
 ﻿using EasyToEnter.ASP.Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EasyToEnter.ASP.ViewsModels.Applicant
 {
     public class VariabilitySelectionContainerViewModel
     {
         public readonly List<VariabilityModel> VariabilityList;
+        public readonly List<SelectListItem> FormSelectList;
+        public readonly List<SelectListItem> FormatSelectList;
+        public readonly List<SelectListItem> PaymentSelectList;
+        public readonly List<SelectListItem> EntranceExamsSelectList;
         public readonly int LevelFocusId;
 
-        public VariabilitySelectionContainerViewModel(List<VariabilityModel> variabilityList, int levelFocus)
+        public VariabilitySelectionContainerViewModel(List<VariabilityModel> variabilityList, List<SelectListItem> formSelectList, List<SelectListItem> formatSelectList, List<SelectListItem> paymentSelectList, List<SelectListItem> entranceExamsSelectList, int levelFocus)
         {
             VariabilityList = variabilityList;
+            FormSelectList = formSelectList;
+            FormatSelectList = formatSelectList;
+            PaymentSelectList = paymentSelectList;
+            EntranceExamsSelectList = entranceExamsSelectList;
             LevelFocusId = levelFocus;
         }
     }
