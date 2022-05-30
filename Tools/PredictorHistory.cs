a0c110106bd9f0387b899cc6594e7b7a6f9a0fef
isooterkin <isooterkin@outlook.com>
2022-05-30 18:28:14 +0300

    Предсказание

---
+++ Tools/PredictorHistory.cs
@@ -0,0 +1,18 @@
+﻿using EasyToEnter.ASP.Models.Models;
+
+namespace EasyToEnter.ASP.Tools
+{
+    public static class PredictorHistory
+    {
+        public static HistoryVariabilityModel Predict(List<HistoryVariabilityModel> historyVariabilityList)
+        {
+            return new HistoryVariabilityModel 
+            {
+                Year = DateTime.Now.Year + 1,
+                Tuition = (int) (historyVariabilityList.Average(hv => hv.Tuition) * 1.1),
+                NumberSeats = (int) (historyVariabilityList.Average(hv => hv.NumberSeats) * 1.1),
+                PassingGrade = (int) (historyVariabilityList.Average(hv => hv.PassingGrade) * 1.1)
+            };
+        }
+    }
+}
