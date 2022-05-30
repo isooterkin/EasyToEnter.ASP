668a2af002539749ef10698794a4527feb0d3b4c
isooterkin <isooterkin@outlook.com>
2022-05-30 18:14:06 +0300

    Хронология

---
+++ Components/History/HistoryCardViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using EasyToEnter.ASP.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyToEnter.ASP.Components.History
+{
+    [ViewComponent]
+    public class HistoryCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(HistoryVariabilityModel data) => View(data);
+    }
+}
---
+++ Components/History/HistoryContainerViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using EasyToEnter.ASP.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyToEnter.ASP.Components.History
+{
+    [ViewComponent]
+    public class HistoryContainerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<HistoryVariabilityModel> data) => View(data);
+    }
+}
