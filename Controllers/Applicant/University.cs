31b6cc5ad455dfbb33e7addb5982c24baab530ba
isooterkin <isooterkin@outlook.com>
2022-05-31 19:08:07 +0300

    Исправил баг

---
+++ Controllers/Applicant/University.cs
@@ -0,0 +1,47 @@
+﻿using EasyToEnter.ASP.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyToEnter.ASP.Controllers.Applicant
+{
+    public partial class ApplicantController
+    {
+        public IActionResult University([FromQuery(Name = "university")] int university)
+        {
+            if (university <= 0) return NotFound();
+
+            // "Вариативность"
+            VariabilityModel? variabilityModel = _context.Variability
+                .Include(v => v.FormatModel)
+                .Include(v => v.FormModel)
+                .Include(v => v.PaymentModel)
+                .Include(v => v.HistoryVariabilitys)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.DisciplineFocusUniversitys)
+                        !.ThenInclude(dfu => dfu!.DisciplineModel)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.LevelFocusModel)
+                        .ThenInclude(lf => lf!.LevelModel)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.LevelFocusModel)
+                        .ThenInclude(lf => lf!.FocusModel)
+                            .ThenInclude(f => f!.DirectionModel)
+                                .ThenInclude(d => d!.GroupModel)
+                                    .ThenInclude(g => g!.ScienceModel)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.UniversityModel)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.SubjectFocusUniversitys)
+                        !.ThenInclude(sfu => sfu!.SubjectModel)
+                .Include(v => v.FocusUniversityModel)
+                    .ThenInclude(fu => fu!.SubjectFocusUniversitys)
+                        !.ThenInclude(sfu => sfu!.SubjectReplacements)
+                            !.ThenInclude(sr => sr!.SubjectModel)
+                .FirstOrDefault(v => v.Id == university);
+
+            if (variabilityModel == null) return NotFound();
+
+            return View(variabilityModel);
+        }
+    }
+}
