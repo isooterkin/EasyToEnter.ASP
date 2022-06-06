69fefdd68b1b6e7741bd12fd522c948bc0f2b5e4
isooterkin <isooterkin@outlook.com>
2022-06-06 04:07:38 +0300

    Баг с паролем

--- ViewsModels/Authentication/SingInViewModel.cs
+++ ViewsModels/Authentication/SingInViewModel.cs
@@ -10,6 +10,7 @@ namespace EasyToEnter.ASP.ViewsModels.Authentication
     {
         [Required(ErrorMessage = "Укажите логин.")]
         [Display(Name = "Логин")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Длина логина должна быть от 4 до 50 символов.")]
         [JsonPropertyName(nameof(Login))]
         public string Login { get; set; }
 
@@ -17,6 +18,7 @@ namespace EasyToEnter.ASP.ViewsModels.Authentication
 
         [Required(ErrorMessage = "Укажите пароль.")]
         [Display(Name = "Пароль")]
+        [StringLength(int.MaxValue, MinimumLength = 8, ErrorMessage = "Длина пароля должна быть от 8 символов.")]
         [JsonPropertyName(nameof(Password))]
         public string Password { get; set; }
     }
