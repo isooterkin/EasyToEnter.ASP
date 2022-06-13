858ff85645110cd36deffca6053cdbb697a6f1ca
isooterkin <isooterkin@outlook.com>
2022-06-14 02:31:19 +0300

    Изменил навигационное меню

---
+++ Controllers/EmployeeOrganization/EmployeeOrganizationController.cs
@@ -0,0 +1,22 @@
+﻿using EasyToEnter.ASP.Data;
+using EasyToEnter.ASP.Tools.Authorization.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyToEnter.ASP.Controllers.EmployeeOrganization
+{
+    public class EmployeeOrganizationController : Controller
+    {
+        private readonly EasyToEnterDbContext _context;
+        public EmployeeOrganizationController(EasyToEnterDbContext context) => _context = context;
+
+
+
+        [HttpGet]
+        [Authorized]
+        //[EmployeeOrganization]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EmployeeUniversity/EmployeeUniversityController.cs
@@ -0,0 +1,22 @@
+﻿using EasyToEnter.ASP.Data;
+using EasyToEnter.ASP.Tools.Authorization.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyToEnter.ASP.Controllers.EmployeeUniversity
+{
+    public class EmployeeUniversityController : Controller
+    {
+        private readonly EasyToEnterDbContext _context;
+        public EmployeeUniversityController(EasyToEnterDbContext context) => _context = context;
+
+
+
+        [HttpGet]
+        [Authorized]
+        //[EmployeeUniversity]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
